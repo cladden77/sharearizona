687f367fac0d771cb71d55f76c65df69f8c75750
Chris L <chris.ladden@gmail.com>
2022-05-05 21:20:52 -0500

    initial setup for three-column layout

---
+++ resources/views/flexible-content/three-col.blade.php
@@ -0,0 +1,15 @@
+<section class="three-col">
+    <div class="container p-5">
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col">
+
+            </div>
+            <div class="col">
+
+            </div>
+        </div>
+    </div>
+</section>
