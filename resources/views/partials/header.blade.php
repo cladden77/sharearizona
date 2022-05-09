b1a20e36cb98b8877f610c3d981e0d03d6ca9a32
Chris L <chris.ladden@gmail.com>
2022-05-08 20:21:59 -0500

    adjusted header nav section for mobile

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,10 @@
 <header class="banner">
     <nav class="navbar sticky-top navbar-expand-lg py-3">
       <div class="container">
-      <a class="navbar-brand text-center" href="{{ home_url('/') }}"><img alt="Share Arizona" src="app/uploads/2022/05/sharearizona-logo.png" /></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="{{ home_url('/') }}"><img alt="Share Arizona" src="/app/uploads/2022/05/sharearizona-logo.png" /></a>
+        {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-        </button>
+        </button> --}}
           @if (has_nav_menu('primary_navigation'))
             {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
           @endif
